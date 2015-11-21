5879707c845321a4a3215ce2549c9fa15a23734f
darrenswhite <darrenswhite95@gmail.com>
2015-11-21 18:32:56 +0000

    Finished documentation, analysis and self evaluation

--- DEPQTest.java
+++ DEPQTest.java
@@ -1,209 +1,233 @@
 package cs21120.depq;
 
-import java.util.ArrayList;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Some basics tests of a Double Ended Priority Queue interface DEPQ
  * You will need to change the setUp method to create an instance of your class that implements the interface.
  * Feel free to add more tests to ensure your solution is working as you expect.
- * 
+ * <p>
  * ArrayList is only used here for testing, do not use it, or any classes from java.util, in your implementation
- * 
+ *
  * @author bpt
  */
+@SuppressWarnings("SuspiciousMethodCalls")
 public class DEPQTest {
-    DEPQ depq;
-    
-    
-    @Before
-    public void setUp() {
-        depq = new Daw48DEPQ(); //TODO, Replace with constructor for your implemenation
-    }
-    
-    
-    /**
-     * Test of inspectLeast method, of class DEPQ.
-     */
-    @Test
-    public void testInspectLeast() {
-        System.out.println("inspectLeast");
-        
-        ArrayList<Integer> array = new ArrayList<Integer>();
-        int smallest =10000;
-        // First check adding random number gives correct smallest
-        for (int i=0; i<100; i++) {
-            int k = (int)(Math.random()*100);
-            array.add(k);
-            depq.add(k);
-            if (k<smallest) smallest = k;
-            
-            Integer result = (Integer)depq.inspectLeast();
-            assertEquals(smallest, result.intValue());
-        
-        }
-        
-        // Next randomly add or remove and check inspect least
-        for (int i=0; i<99; i++) {
-            boolean add = Math.random()>0.5;
-            if (add) {
-                int k = (int)(Math.random()*100);
-                array.add(k);
-                depq.add(k);
-                if (k<smallest) smallest = k;
-            } else {
-                Integer discarded = (Integer)depq.getLeast();
-                array.remove(discarded);
-                smallest = array.get(0);
-                for(int j=1; j<array.size(); j++) {
-                    if (array.get(j)<smallest) smallest = array.get(j);
-                }
-            }
-            
-            Integer result = (Integer)depq.inspectLeast();
-            assertEquals(smallest, result.intValue());
-        }
-    }
-    /**
-     * Test of inspectMost method, of class DEPQ.
-     */
-    @Test
-    public void testInspectMost() {
-        System.out.println("inspectMost");
-        
-        ArrayList<Integer> array = new ArrayList<Integer>();
-        int largest = -10000;
-        // First check adding random number gives correct largest
-        for (int i=0; i<100; i++) {
-            int k = (int)(Math.random()*100);
-            array.add(k);
-            depq.add(k);
-            if (k>largest) largest = k;
-            
-            Integer result = (Integer)depq.inspectMost();
-            assertEquals(largest, result.intValue());
-        
-        }
-        
-        // Next randomly add or remove and check inspect most
-        for (int i=0; i<99; i++) {
-            boolean add = Math.random()>0.5;
-            if (add) {
-                int k = (int)(Math.random()*100);
-                array.add(k);
-                depq.add(k);
-                if (k>largest) largest = k;
-            } else {
-                Integer discarded = (Integer)depq.getMost();
-                array.remove(discarded);
-                largest = array.get(0);
-                for(int j=1; j<array.size(); j++) {
-                    if (array.get(j)>largest) largest = array.get(j);
-                }
-            }
-            
-            Integer result = (Integer)depq.inspectMost();
-            assertEquals(largest, result.intValue());
-        }
-    }
 
-    /**
-     * Test of add method, of class DEPQ.
-     */
-    @Test
-    public void testAdd() {
-        System.out.println("add");
-        
-        for (int i=0; i<1000; i++) {
-            int k = (int)(Math.random()*100);
-            depq.add(k);
-            assertEquals(i+1, depq.size());
-        }
-        
-    }
+	private DEPQ depq;
 
-    /**
-     * Test of getLeast method, of class DEPQ.
-     */
-    @Test
-    public void testGetLeast() {
-        System.out.println("getLeast");
-        
-        for (int i=0; i<1000; i++) {
-            depq.add((int)(Math.random()*100));
-        }
-        for (int i=0; i<1000; i++) {
-            Integer expResult = (Integer)depq.inspectLeast();
-            Integer result = (Integer)depq.getLeast();
-            assertEquals(expResult, result);
-        }
-    }
+	@Before
+	public void setUp() {
+		depq = new Daw48DEPQ();
+	}
 
-    /**
-     * Test of getMost method, of class DEPQ.
-     */
-    @Test
-    public void testGetMost() {
-        System.out.println("getMost");
-        
-        for (int i=0; i<1000; i++) {
-            depq.add((int)(Math.random()*100));
-        }
-        for (int i=0; i<1000; i++) {
-            Integer expResult = (Integer)depq.inspectMost();
-            Integer result = (Integer)depq.getMost();
-            assertEquals(expResult, result);
-        }
-    }
+	/**
+	 * Test of add method, of class DEPQ.
+	 */
+	@Test
+	public void testAdd() {
+		System.out.println("add");
 
-    /**
-     * Test of isEmpty method, of class DEPQ.
-     */
-    @Test
-    public void testIsEmpty() {
-        System.out.println("isEmpty");
-        
-        boolean expResult = true;
-        boolean result = depq.isEmpty();
-        assertEquals(expResult, result);
-        
-        for (int i=0; i<10; i++) {
-            int count = (int)(Math.random()*1000);
-            for (int j=0; j<count; j++) {
-                depq.add(j);
-                assertEquals(false, depq.isEmpty());
-            }
-            for (int j=0; j<count; j++) {
-                assertEquals(false, depq.isEmpty());
-                depq.getLeast();
-            }
-            assertEquals(true, depq.isEmpty());
-        }
-    }
+		for (int i = 0; i < 1000; i++) {
+			depq.add((int) (Math.random() * 100));
 
-    /**
-     * Test of size method, of class DEPQ.
-     */
-    @Test
-    public void testSize() {
-        System.out.println("size");
-        
-        for (int i=0; i<1000; i++) {
-            int k = (int)(Math.random()*100);
-            depq.add(k);
-            assertEquals(i+1, depq.size());
-        }
-        
-        for (int i=0; i<1000; i++) {
-            boolean bigEnd = Math.random()>0.5;
-            if (bigEnd) depq.getMost();
-            else depq.getLeast();
-            assertEquals(1000-i-1, depq.size());
-        }
-    }
+			assertEquals(i + 1, depq.size());
+		}
+	}
+
+	/**
+	 * Test of getLeast method, of class DEPQ.
+	 */
+	@Test
+	public void testGetLeast() {
+		System.out.println("getLeast");
+
+		for (int i = 0; i < 1000; i++) {
+			depq.add((int) (Math.random() * 100));
+		}
+
+		for (int i = 0; i < 1000; i++) {
+			assertEquals(depq.inspectLeast(), depq.getLeast());
+		}
+	}
+
+	/**
+	 * Test of getMost method, of class DEPQ.
+	 */
+	@Test
+	public void testGetMost() {
+		System.out.println("getMost");
+
+		for (int i = 0; i < 1000; i++) {
+			depq.add((int) (Math.random() * 100));
+		}
+
+		for (int i = 0; i < 1000; i++) {
+			assertEquals(depq.inspectMost(), depq.getMost());
+		}
+	}
+
+	/**
+	 * Test of inspectLeast method, of class DEPQ.
+	 */
+	@Test
+	public void testInspectLeast() {
+		System.out.println("inspectLeast");
+
+		List<Integer> array = new ArrayList<>();
+		int smallest = 10000;
+
+		// First check adding random number gives correct smallest
+		for (int i = 0; i < 100; i++) {
+			int k = (int) (Math.random() * 100);
+
+			array.add(k);
+			depq.add(k);
+
+			if (k < smallest) {
+				smallest = k;
+			}
+
+			assertEquals(smallest, depq.inspectLeast());
+		}
+
+		// Next randomly add or remove and check inspect least
+		for (int i = 0; i < 99; i++) {
+			boolean add = Math.random() > 0.5;
+
+			if (add) {
+				int k = (int) (Math.random() * 100);
+
+				array.add(k);
+				depq.add(k);
+
+				if (k < smallest) {
+					smallest = k;
+				}
+			} else {
+				array.remove(depq.getLeast());
+				smallest = array.get(0);
+
+				for (int j = 1; j < array.size(); j++) {
+					if (array.get(j) < smallest) {
+						smallest = array.get(j);
+					}
+				}
+			}
+
+			assertEquals(smallest, depq.inspectLeast());
+		}
+	}
+
+	/**
+	 * Test of inspectMost method, of class DEPQ.
+	 */
+	@Test
+	public void testInspectMost() {
+		System.out.println("inspectMost");
+
+		List<Integer> array = new ArrayList<>();
+		int largest = -10000;
+
+		// First check adding random number gives correct largest
+		for (int i = 0; i < 100; i++) {
+			int k = (int) (Math.random() * 100);
+
+			array.add(k);
+			depq.add(k);
+
+			if (k > largest) {
+				largest = k;
+			}
+
+			assertEquals(largest, depq.inspectMost());
+		}
+
+		// Next randomly add or remove and check inspect most
+		for (int i = 0; i < 99; i++) {
+			boolean add = Math.random() > 0.5;
+
+			if (add) {
+				int k = (int) (Math.random() * 100);
+
+				array.add(k);
+				depq.add(k);
+
+				if (k > largest) {
+					largest = k;
+				}
+			} else {
+				array.remove(depq.getMost());
+				largest = array.get(0);
+
+				for (int j = 1; j < array.size(); j++) {
+					if (array.get(j) > largest) {
+						largest = array.get(j);
+					}
+				}
+			}
+
+			assertEquals(largest, depq.inspectMost());
+		}
+	}
+
+	/**
+	 * Test of isEmpty method, of class DEPQ.
+	 */
+	@Test
+	public void testIsEmpty() {
+		System.out.println("isEmpty");
+
+		assertEquals(true, depq.isEmpty());
+
+		for (int i = 0; i < 10; i++) {
+			int count = (int) (Math.random() * 1000);
+
+			for (int j = 0; j < count; j++) {
+				depq.add(j);
+
+				assertEquals(false, depq.isEmpty());
+			}
+
+			for (int j = 0; j < count; j++) {
+				assertEquals(false, depq.isEmpty());
+
+				depq.getLeast();
+			}
+
+			assertEquals(true, depq.isEmpty());
+		}
+	}
+
+	/**
+	 * Test of size method, of class DEPQ.
+	 */
+	@Test
+	public void testSize() {
+		System.out.println("size");
+
+		for (int i = 0; i < 1000; i++) {
+			depq.add((int) (Math.random() * 100));
+
+			assertEquals(i + 1, depq.size());
+		}
+
+		for (int i = 0; i < 1000; i++) {
+			boolean bigEnd = Math.random() > 0.5;
+
+			if (bigEnd) {
+				depq.getMost();
+			} else {
+				depq.getLeast();
+			}
+
+			assertEquals(1000 - i - 1, depq.size());
+		}
+	}
 }
--- Daw48DEPQ.java
+++ Daw48DEPQ.java
@@ -1,9 +1,61 @@
 package cs21120.depq;
 
-import java.util.NoSuchElementException;
-
 /**
- * TODO
+ * This implementation of a Double-ended Priority Queue uses an interval heap to
+ * store and retrieve elements. Elements can be added using the add(Comparable)
+ * method and can be inspected as well as inspected and removed. Further detail
+ * for these methods can be found in the JavaDoc comments (as well as code
+ * comments) below. The interval heap is similar to a min-max heap but
+ * combines both of them into one. This means that each node in the heap contains
+ * two elements and is also a binary tree. These two elements in the node are
+ * referred to as a closed interval. The left element must be smaller than the
+ * right element (these elements are also known as min and max) but if there is
+ * only one element in the interval then there are no boundary constraints. Each
+ * child interval must be within the bounds of its parent interval. The root
+ * interval will also contain the min and max elements due to how the heap is
+ * structured. All of the elements are stored in nodes (each node holds two) and
+ * all nodes are stored in an array (resized when needed). The finer technical
+ * details are within the JavaDoc comments for each method. This implementation
+ * was chosen due to it's fast nature of adding, inspecting and removing elements.
+ * <p>
+ * The time complexity for adding a new element is O(log n) because the greatest
+ * factor in the algorithm (bubbleMaxUp or bubbleMinUp) only use a single while
+ * loop to traverse the tree from the last leaf to the root and the height at
+ * which this leaf node is at is log n. Note that n is the number of elements
+ * and not the number of nodes.
+ * <p>
+ * The time complexity for getting the 'least' or 'most' element is O(log n)
+ * because again the greatest time factor here is the while loop which iterates
+ * from the root to the bottom of the tree.
+ * <p>
+ * Inspecting the 'least' or 'most' element is O(1) because the simply accesses
+ * the array of nodes (which is constant) and return the correct element.
+ * <p>
+ * isEmpty and size also have a time complexity of O(1) because they are simple
+ * constant expressions which only access the size variable to determine the
+ * size of the array (and compare it to 0 for isEmpty).
+ * <p>
+ * Correct functioning of each method (25%): Each of the methods implemented
+ * work as intended each time (tested with JUnit tests) and the queue is able
+ * to hold at least 1000 items so I would award my self 25 marks for this
+ * section).
+ * <p>
+ * Overall efficiency (25%): The solution used has O(log n) complexity for
+ * the add, getMost and getLeast methods, and the other methods have O(1). I
+ * would award myself 25 marks for this section as I the methods implemented
+ * have time complexities of that required.
+ * <p>
+ * Documentation based report (25%): I have properly documented each method
+ * using JavaDoc and have supplied necessary comments to the code. I have used
+ * references but not cited them as I used them for a general overview of how
+ * interval heaps work. I would award myself 20 marks for this due to not
+ * citing any references (only listing them instead).
+ * <p>
+ * Complexity Analysis (20%): I have included an analysis of my implementation
+ * for each method (in the DEPQ interface). I was not fully confident of how
+ * to do this so I don't think I did very well here so I would award myself 10.
+ * <p>
+ * Overall I would award myself 85 marks.
  * <p>
  * References:
  * <p>
@@ -41,7 +93,17 @@ public class Daw48DEPQ implements DEPQ {
 	}
 
 	/**
-	 * TODO
+	 * Adds a new Comparable element into the DEPQ in the correct position.
+	 * If the last node only has one element in it then the new element is
+	 * inserted correctly into that node otherwise a new node is created
+	 * with the new element and the new node is inserted into the end of the
+	 * array, expanding the size of the array when needed. If there is 2 or less
+	 * elements then we do not need to resort the heap otherwise the newly
+	 * inserted element needs to be moved to its correct position in the heap.
+	 * we do this by bubbling up the element using bubbleMinUp() or
+	 * bubbleMaxUp() depending which element it was inserted as (left = min,
+	 * right = max). No null elements can be added and will throw a
+	 * NullPointerException.
 	 *
 	 * @param c the element to insert into the DEPQ
 	 */
@@ -123,7 +185,13 @@ public class Daw48DEPQ implements DEPQ {
 	}
 
 	/**
-	 * TODO
+	 * Bubbles the max element up the heap until it is in the correct
+	 * position. This will compare the last nodes max (right) element with
+	 * its parent max (right) element and will swap them if the parent node
+	 * max element is smaller than the last node max element. This is then
+	 * repeated on the next parent node up the heap until the node is in the
+	 * correct position where its parent max element is larger than its max
+	 * element and its children's max elements are smaller than it.
 	 */
 	private void bubbleMaxUp() {
 		// The current index of node to shift up, start with the last element
@@ -174,7 +242,13 @@ public class Daw48DEPQ implements DEPQ {
 	}
 
 	/**
-	 * TODO
+	 * Bubbles the min element up the heap until it is in the correct
+	 * position. This will compare the last nodes min (left) element with
+	 * its parent min (left) element and will swap them if the parent node
+	 * min element is larger than the last node min element. This is then
+	 * repeated on the next parent node up the heap until the node is in the
+	 * correct position where its parent min element is smaller than its min
+	 * element and its children's min elements are larger than it.
 	 */
 	private void bubbleMinUp() {
 		// The current index of node to shift up, start with the last element
@@ -211,13 +285,27 @@ public class Daw48DEPQ implements DEPQ {
 	}
 
 	/**
-	 * TODO
+	 * Gets the smallest element in the DEPQ and removes it from the DEPQ. The
+	 * smallest element is retrieved using inspectLeast(); this is the element
+	 * which will be returned after removing and ensuring that the heap is
+	 * correct and if not swapping elements where needed. If there is only
+	 * one element in the queue then it is removed (and its node) and returned.
+	 * Otherwise, the last nodes min (left) element is moved into the root node
+	 * and the last node left element is removed and replaced with the max (right)
+	 * element if there is one. Now, starting at the root node, we compare both
+	 * children to get the child with the smallest min (left) element. Next we
+	 * compare this child's left element with the nodes left element and if
+	 * the child left element is smaller then we swap them otherwise the element
+	 * is in the correct position and we can stop. We continue this while the
+	 * current node has a child or until the node is in the correct position,
+	 * where the child node min (left) element is larger than the nodes min
+	 * (left) element.
 	 *
 	 * @return returns the smallest element in the DEPQ
 	 */
 	@Override
 	public Comparable getLeast() {
-		// Get the least element - may throw NoSuchElementException if empty
+		// Get the least element - may be null if queue is empty
 		Comparable min = inspectLeast();
 
 		// If it is the only element, remove the node and return it
@@ -312,14 +400,32 @@ public class Daw48DEPQ implements DEPQ {
 	}
 
 	/**
-	 * TODO
+	 * Gets the largest element in the DEPQ and removes it from the DEPQ. The
+	 * largest element is retrieved using inspectMost(); this is the element
+	 * which will be returned after removing and ensuring that the heap is
+	 * correct and if not swapping elements where needed. If there is only
+	 * one element in the queue then it is removed (and its node) and returned.
+	 * Otherwise, the last nodes min (left) element is moved into the root node
+	 * and the last node left element is removed and replaced with the max (right)
+	 * element if there is one. Now, starting at the root node, we compare both
+	 * children to get the child with the smallest min (left) element. Next we
+	 * compare this child's left element with the nodes left element and if
+	 * the child left element is smaller then we swap them otherwise the element
+	 * is in the correct position and we can stop. We continue this while the
+	 * current node has a child or until the node is in the correct position,
+	 * where the child node min (left) element is larger than the nodes min
+	 * (left) element.
 	 *
 	 * @return returns the largest element in the DEPQ
 	 */
 	@Override
 	public Comparable getMost() {
-		// Get the most element - may throw NoSuchElementException if empty
+		// Get the most element - may be null if queue is empty
 		Comparable max = inspectMost();
+
+		if (max == null) {
+			return null;
+		}
 
 		// If it is the only element, remove the node and return it
 		// Remember to decrease number of elements as well
@@ -413,24 +519,25 @@ public class Daw48DEPQ implements DEPQ {
 	}
 
 	/**
-	 * TODO
+	 * Gets the smallest element in the DEPQ but does not remove it from
+	 * the DEPQ. This will simply access the nodes array and retrieve the min
+	 * element from the root node. Null may be returned if the queue is empty.
 	 *
 	 * @return returns the smallest element in the DEPQ
 	 */
 	@Override
 	public Comparable inspectLeast() {
-		// No elements in the queue so there
-		// is no least value
-		if (numElements == 0) {
-			throw new NoSuchElementException("DEPQ is empty");
-		}
-
-		// The least value is the left element of the root node
-		return nodes[0].left;
+		// If there are elements in the queue
+		// then return the least value is the
+		// left element of the root node otherwise
+		// return null
+		return numElements > 0 ? nodes[0].left : null;
 	}
 
 	/**
-	 * TODO
+	 * Gets the largest element in the DEPQ but does not remove it from
+	 * the DEPQ. This will simply access the nodes array and retrieve the max
+	 * element from the root node. Null may be returned if the queue is empty.
 	 *
 	 * @return returns the largest element in the DEPQ
 	 */
@@ -439,7 +546,7 @@ public class Daw48DEPQ implements DEPQ {
 		// No elements in the queue so there
 		// is no most value
 		if (numElements == 0) {
-			throw new NoSuchElementException("DEPQ is empty");
+			return null;
 		}
 
 		// The most value is the right element of the root node
@@ -451,18 +558,22 @@ public class Daw48DEPQ implements DEPQ {
 	}
 
 	/**
-	 * TODO
+	 * Checks if the DEPQ is empty which is only true when the number
+	 * of elements is 0 (also the number of nodes will be 0)
 	 *
 	 * @return returns true if the queue is empty
 	 */
 	@Override
 	public boolean isEmpty() {
 		// The queue is empty if it contains 0 elements
+		// We could also check the number of nodes but this is redundant
 		return numElements == 0;
 	}
 
 	/**
-	 * TODO
+	 * Gets the number of elements in the DEPQ. This is not the number
+	 * of nodes. This simply increments when adding and decrements when
+	 * removing elements.
 	 *
 	 * @return returns the number of elements currently in the DEPQ
 	 */
